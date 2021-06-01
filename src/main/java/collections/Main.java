b9ec49985bcb4e391992dbedd7cca3ccdccb80ca
Lucas Ramon <78308822+lramon2001@users.noreply.github.com>
2021-06-01 15:37:58 -0300

    Adicionando a classe pessoa

--- src/main/java/collections/Main.java
+++ src/main/java/collections/Main.java
@@ -1,10 +1,17 @@
 package collections;
 
+import modelos.Pessoa;
+
+import javax.swing.*;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-
+          List<Pessoa> pessoas = new ArrayList<Pessoa>();
+          pessoas.add(new Pessoa(1,"Lucas"));
+          pessoas.add(new Pessoa(2,"Pedro"));
+        JOptionPane.showMessageDialog(null,pessoas.toString());
     }
 }
