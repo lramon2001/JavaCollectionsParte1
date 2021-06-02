b48bfc9df7d96cbdaec07c64c6bac3cfeee31cc3
Lucas Ramon <78308822+lramon2001@users.noreply.github.com>
2021-06-01 21:06:49 -0300

    Começando iterators

--- src/main/java/collections/Main.java
+++ src/main/java/collections/Main.java
@@ -5,6 +5,7 @@ import modelos.Pessoa;
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 public class Main {
@@ -12,6 +13,14 @@ public class Main {
           List<Pessoa> pessoas = new ArrayList<Pessoa>();
           pessoas.add(new Pessoa(1,"Lucas"));
           pessoas.add(new Pessoa(2,"Pedro"));
+          pessoas.add(4,new Pessoa(5,"João"));
+          pessoas.contains(new Pessoa(1,"Lucas"));
         JOptionPane.showMessageDialog(null,pessoas.toString());
+        JOptionPane.showMessageDialog(null,"Agora usaremos o Iterator para printar os nomes");
+        Iterator<Pessoa> interadorDePessoas = pessoas.iterator();
+        while(interadorDePessoas.hasNext()){
+            Pessoa p = interadorDePessoas.next();
+            JOptionPane.showMessageDialog(null,p.toString());
+        }
     }
 }
