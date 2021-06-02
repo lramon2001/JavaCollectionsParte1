7d582d2f01e47838aee8bd55f933f8e7ed9e8b99
Lucas Ramon <78308822+lramon2001@users.noreply.github.com>
2021-06-01 23:36:01 -0300

    Treinando mais iterators

--- src/main/java/collections/Main.java
+++ src/main/java/collections/Main.java
@@ -3,23 +3,40 @@ package collections;
 import modelos.Pessoa;
 
 import javax.swing.*;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args){
           List<Pessoa> pessoas = new ArrayList<Pessoa>();
           pessoas.add(new Pessoa(1,"Lucas"));
           pessoas.add(new Pessoa(2,"Pedro"));
+          pessoas.add(new Pessoa(3,"Judas"));
           pessoas.contains(new Pessoa(1,"Lucas"));
         JOptionPane.showMessageDialog(null,pessoas.toString());
         JOptionPane.showMessageDialog(null,"Agora usaremos o Iterator para printar os nomes");
+
+        List<Pessoa> pessoasListaLigada = new LinkedList<Pessoa>();
+        pessoasListaLigada.add(new Pessoa(1,"Mateus"));
+        pessoasListaLigada.add(new Pessoa(2,"João"));
+        pessoasListaLigada.add(new Pessoa(2,"Tiago"));
+        pessoasListaLigada.add(new Pessoa(2,"Tiago"));
+
         Iterator<Pessoa> interadorDePessoas = pessoas.iterator();
         while(interadorDePessoas.hasNext()){
             Pessoa p = interadorDePessoas.next();
+            if(p.getId()==1){
+                interadorDePessoas.remove();
+            }
             JOptionPane.showMessageDialog(null,p.toString());
+        }
+
+        JOptionPane.showMessageDialog(null,"Com for each");
+        for(Pessoa p : pessoas){
+            if(p.getId()==1){
+                pessoas.remove(p);
+            }
+            JOptionPane.showMessageDialog(null,p.toString());
+
         }
     }
 }
---
+++ src/main/java/collections/Main2.java
@@ -0,0 +1,27 @@
+package collections;
+
+import modelos.Pessoa;
+
+import javax.swing.*;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main2 {
+    public static void main(String[] args){
+        List<Pessoa> pessoas = new LinkedList<Pessoa>();
+        pessoas.add(new Pessoa(1,"A1"));
+        pessoas.add(new Pessoa(2,"A2"));
+        pessoas.add(new Pessoa(3,"A3"));
+        JOptionPane.showMessageDialog(null,"Com Interator");
+        Iterator<Pessoa> iteratorPessoa = pessoas.iterator();
+        while(iteratorPessoa.hasNext()){
+            Pessoa p = iteratorPessoa.next();
+            if(p.getId()==1){
+                iteratorPessoa.remove();
+            }
+            JOptionPane.showMessageDialog(null,p.toString());
+        }
+        JOptionPane.showMessageDialog(null,pessoas);
+    }
+}
